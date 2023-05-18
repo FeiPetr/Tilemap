1e2f73a67d622c39faae7f39f235b3b006f67017
FeiPetr <777spetrova@gmail.com>
2023-05-18 00:51:47 -0700

    adding sprite

--- src/Overworld.js
+++ src/Overworld.js
@@ -2,6 +2,7 @@ class Overworld extends Phaser.Scene{
     constructor()
     {
         super({key:'overworldScene'})
+        this.VEL = 100
     }
     preload(){
         this.load.path = "./assets/"
@@ -20,8 +21,46 @@ class Overworld extends Phaser.Scene{
         const terrainLayer = map.createLayer('Terrain',tileset,0,0)
         const treeLayer = map.createLayer('Trees',tileset,0,0)
 
+        //add character
+        this.slime = this.physics.add.sprite(32,32,'slime',0)
+        this.anims.create({
+            key: 'jiggle',
+            frameRate: 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime',{
+                start:0,
+                end:1
+            })
+        })
+        this.slime.play('jiggle')
+        this.slime.body.setCollideWorldBounds(true)
+        //input
+        this.cursors = this.input.keybaord.createCursorKeys()
+
+
+
     }
     update(){
+        this.direction = new Phaser.Math.Vector2(0)
+        if(this.cursor.left.isDown)
+        {
+            this.direction.x = -1
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.direction.x = 1
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.direction.y = 1
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.direction.y = 1
+        }
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL*this.direction.x,this.VEL*this.direction.y)
+
         
     }
 }
