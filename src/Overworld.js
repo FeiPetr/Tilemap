499c6aec5f353f64253972726ea3a1667dee21d2
FeiPetr <777spetrova@gmail.com>
2023-05-18 01:25:33 -0700

    bug fixing

--- src/Overworld.js
+++ src/Overworld.js
@@ -5,8 +5,8 @@ class Overworld extends Phaser.Scene{
         this.VEL = 100
     }
     preload(){
-        this.load.path = "./assets/"
-        this.load.spritesheet = ('slime','slime.png',{
+        this.load.path = './assets/'
+        this.load.spritesheet('slime','slime.png',{
             frameWidth: 16,
             frameHeight:16
         })
@@ -19,30 +19,35 @@ class Overworld extends Phaser.Scene{
         // add layer
         const bgLayer = map.createLayer('Background',tileset,0,0)
         const terrainLayer = map.createLayer('Terrain',tileset,0,0)
-        const treeLayer = map.createLayer('Trees',tileset,0,0)
+        const treeLayer = map.createLayer('Trees',tileset,0,0);
 
         //add character
-        this.slime = this.physics.add.sprite(32,32,'slime',0)
+        this.slime = this.physics.add.sprite(32, 32,'slime',0);
+
         this.anims.create({
             key: 'jiggle',
-            frameRate: 8,
+            frameRate: 6,
             repeat: -1,
             frames: this.anims.generateFrameNumbers('slime',{
                 start:0,
                 end:1
             })
-        })
+        });
         this.slime.play('jiggle')
         this.slime.body.setCollideWorldBounds(true)
+
+        this.cameras.main.setBounds(0,0,map.widthInPixels,map.heightInPixels)
+        this.cameras.main.startFollow(this.slime,true,0.25,0.25)
+        this.physics.world.bounds.setTo(0,0,map.widthInPixels,map.heightInPixels)
+
+
         //input
-        this.cursors = this.input.keybaord.createCursorKeys()
-
-
+        this.cursors = this.input.keyboard.createCursorKeys()
 
     }
     update(){
         this.direction = new Phaser.Math.Vector2(0)
-        if(this.cursor.left.isDown)
+        if(this.cursors.left.isDown)
         {
             this.direction.x = -1
         }
@@ -50,9 +55,9 @@ class Overworld extends Phaser.Scene{
         {
             this.direction.x = 1
         }
-        else if (this.cursors.up.isDown)
+        if (this.cursors.up.isDown)
         {
-            this.direction.y = 1
+            this.direction.y = -1
         }
         else if (this.cursors.down.isDown)
         {
