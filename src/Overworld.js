7a9e025ab0a03863f94345b8023fde9fa74a5fea
FeiPetr <777spetrova@gmail.com>
2023-05-17 23:55:37 -0700

    Overworld updte

--- src/Overworld.js
+++ src/Overworld.js
@@ -4,9 +4,18 @@ class Overworld extends Phaser.Scene{
         super({key:'overworldScene'})
     }
     preload(){
-
+        this.load.path = "./assets/"
+        this.load.spritesheet = ('slime',slime.png,{
+            frameWidth: 16,
+            frameHeight:16
+        })
+        this.load.image('tilesetImage','tileset.png')
+        this.load.tilemapTiledJSON('tilemapJSON','area01.json')
     }
     create(){
+        const map = this.add.tilemap('tilemapJSON')
+        const tileset = map.addTilesetImage()
+        
 
     }
     update(){
