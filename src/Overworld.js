11c4e3f25f79980160815b058b4c9c185c3bfc13
FeiPetr <777spetrova@gmail.com>
2023-05-18 00:09:07 -0700

    set tiles

--- src/Overworld.js
+++ src/Overworld.js
@@ -5,18 +5,20 @@ class Overworld extends Phaser.Scene{
     }
     preload(){
         this.load.path = "./assets/"
-        this.load.spritesheet = ('slime',slime.png,{
+        this.load.spritesheet = ('slime','slime.png',{
             frameWidth: 16,
             frameHeight:16
         })
         this.load.image('tilesetImage','tileset.png')
-        this.load.tilemapTiledJSON('tilemapJSON','area01.json')
+        this.load.tilemapTiledJSON('tilemapJSON','area1.json')
     }
     create(){
         const map = this.add.tilemap('tilemapJSON')
         const tileset = map.addTilesetImage('tileset','tilesetImage')
         // add layer
         const bgLayer = map.createLayer('Background',tileset,0,0)
+        const terrainLayer = map.createLayer('Terrain',tileset,0,0)
+        const treeLayer = map.createLayer('Trees',tileset,0,0)
 
     }
     update(){
